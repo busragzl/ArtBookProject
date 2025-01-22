bd44e3a8f90ada26002120a43077e0d4d7e9c0d2
Busra Gazel <busragzl96@gmail.com>
2025-01-23 01:33:47 +0300

    ekran tasarımının yapılması ve VC bağlanması

---
+++ ArtBookProject/DetailsVC.swift
@@ -0,0 +1,31 @@
+//
+//  DetailsVC.swift
+//  ArtBookProject
+//
+//  Created by Musa on 23.01.2025.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+    
+   
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+    @IBAction func saveButtonClicked(_ sender: Any) {
+    }
+    
+
+}
