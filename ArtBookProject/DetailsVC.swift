13cdb3e89ef8e3aaaa2ea8dc5c4f2d2fac78b5f6
Busra Gazel <busragzl96@gmail.com>
2025-01-23 22:56:52 +0300

    imaview tıklandığında galeriden fotograf seçme işlemleri

--- ArtBookProject/DetailsVC.swift
+++ ArtBookProject/DetailsVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
    
     @IBOutlet weak var imageView: UIImageView!
@@ -18,15 +18,39 @@ class DetailsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        //Recognizer
 
       let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)) // Similator üzerinde editText tıklandığında diger editTestlerin üzeri kapandığı için
         view.addGestureRecognizer(gestureRecognizer) // view üzerinde neresi tıklanırsa tıklansın keybord kapanması için
+        
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTabRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTabRecognizer)
+        
         
     }
 
     @objc func hideKeyboard() {
         view.endEditing(true) // klavye kapatma
     }
+    @objc func selectImage() {
+        let picker = UIImagePickerController() // imageView tıklantığında fotograf seçebilmek için
+        picker.delegate = self
+        picker.sourceType = .photoLibrary // seçilecek fotografın galeriden alınması
+        picker.allowsEditing = true // seçilecek fotografın editlenmesi
+        present(picker, animated: true, completion: nil) // ekranda alert gibi göstermesi için
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info[.originalImage] as? UIImage // any tipinde dönen resim objesinin UIImage'e dönüştürülmesi
+        self.dismiss(animated: true) // picker'ın kapatılması
+        
+    } // image seçimi bittiğinde any tipinde seçilen görselin döndürülmesi
 
    
     @IBAction func saveButtonClicked(_ sender: Any) {
